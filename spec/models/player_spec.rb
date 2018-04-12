afea1735e2acab02af791effb68f7f45346cfa3f
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-11 17:32:52 -0700

    Change Player starting positions to be constants

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
+Start_pos_x = 10
+Start_pos_y = 10
+
 RSpec.describe Player do
-    let(:startPosX) {10}
-    let(:startPosY) {10}
-    let(:player) {Player.new(startPosX, startPosY)}
+    let(:player) {Player.new(Start_pos_x, Start_pos_y)}
 
     it "is not initially in combat" do
         expect(player.in_combat?).to be_falsy
@@ -12,25 +13,25 @@ RSpec.describe Player do
 
     it "can move forward" do
         player.move_forward
-        expect(player.x).to eq(startPosX)
-        expect(player.y).to eq(startPosY + 1)
+        expect(player.x).to eq(Start_pos_x)
+        expect(player.y).to eq(Start_pos_y + 1)
     end
 
     it "can move back" do
         player.move_back
-        expect(player.x).to eq(startPosX)
-        expect(player.y).to eq(startPosY - 1)
+        expect(player.x).to eq(Start_pos_x)
+        expect(player.y).to eq(Start_pos_y - 1)
     end
 
     it "can move left" do
         player.move_left
-        expect(player.x).to eq(startPosX - 1)
-        expect(player.y).to eq(startPosY)
+        expect(player.x).to eq(Start_pos_x - 1)
+        expect(player.y).to eq(Start_pos_y)
     end
 
     it "can move right" do
         player.move_right
-        expect(player.x).to eq(startPosX + 1)
-        expect(player.y).to eq(startPosY)
+        expect(player.x).to eq(Start_pos_x + 1)
+        expect(player.y).to eq(Start_pos_y)
     end
 end
