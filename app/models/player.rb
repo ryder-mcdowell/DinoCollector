2b2550f56fe22dc7780a39958113ac5520e5c1f2
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-16 10:05:37 -0700

    Remove 2d related Player attributes and methods and associated tests

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,26 +1,7 @@
 class Player
-    attr_accessor :x, :y
 
-    def initialize(x, y)
-        @x = x
-        @y = y
+    def initialize()
         @in_combat = false
-    end
-
-    def move_forward
-        @y += 1
-    end
-
-    def move_back
-        @y -= 1
-    end
-
-    def move_left
-        @x -= 1
-    end
-
-    def move_right
-        @x += 1
     end
 
     def in_combat?
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,37 +1,13 @@
 require 'rails_helper'
 
-Start_pos_x = 10
-Start_pos_y = 10
-
 RSpec.describe Player do
-    let(:player) {Player.new(Start_pos_x, Start_pos_y)}
+    let(:player) {Player.new}
 
     it "is not initially in combat" do
         expect(player.in_combat?).to be_falsy
         #expect(player).not_to be_in_combat
     end
 
-    it "can move forward" do
-        player.move_forward
-        expect(player.x).to eq(Start_pos_x)
-        expect(player.y).to eq(Start_pos_y + 1)
-    end
 
-    it "can move back" do
-        player.move_back
-        expect(player.x).to eq(Start_pos_x)
-        expect(player.y).to eq(Start_pos_y - 1)
-    end
 
-    it "can move left" do
-        player.move_left
-        expect(player.x).to eq(Start_pos_x - 1)
-        expect(player.y).to eq(Start_pos_y)
-    end
-
-    it "can move right" do
-        player.move_right
-        expect(player.x).to eq(Start_pos_x + 1)
-        expect(player.y).to eq(Start_pos_y)
-    end
 end
