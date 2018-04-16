c85ac4c3dd9025000842e088726fb93c392a1e50
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-16 12:04:50 -0700

    Change Player to use ApplicationRecord

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,34 +1,35 @@
-class Player
-  attr_accessor :dinos
+class Player < ApplicationRecord
+  #attr_accessor :dinos
+  has_many :dinos
 
-  def initialize()
-    @in_combat = false
-    @my_turn = false
-    @dinos = []
-  end
+  #def initialize()
+  #  @in_combat = false
+  #  @my_turn = false
+  #  @dinos = []
+  #end
 
   def in_combat?
-    @in_combat
+    in_combat
   end
 
   def initiate_combat
-    @in_combat = true
+    self.in_combat = true
   end
 
   def my_turn?
-    @my_turn
+    my_turn
   end
 
   def make_turn
-    @my_turn = true
+    self.my_turn = true
   end
 
-  def add_dino(dino)
-    @dinos << dino
-  end
+  #def add_dino(dino)
+  #  @dinos << dino
+  #end
 
   def dino_count
-    @dinos.count
+    dinos.count
   end
 
 end
