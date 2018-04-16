65262fa6082ce2be5d9d3d2f1278feeae6c5e570
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-16 12:32:47 -0700

    Add pending tag to pending player system tests

--- spec/system/new_player_spec.rb
+++ spec/system/new_player_spec.rb
@@ -1,11 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "creating a player", type: :system do
-  it "allows a user to create a player with a name" do
+  pending "allows a user to create a player with a name" do
     visit new_user_path
     fill_in "Player Name", with: "Dino_Master_Bob"
     click_on("Create New Player")
     visit player_path
     expect(page).to have_content("Dino_Master_Bob")
+  end
+
+  pending "allows a user to select a starting dino" do
+    visit pick_dino_path
+    click_on("Raptor")
+    visit player_dinos_path
+    expect(page).to have_content("Raptor")
   end
 end
--- spec/workflows/creates_player_spec.rb
+++ spec/workflows/creates_player_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe CreatesPlayer do
-  it "creates a player given a name" do
+  pending "creates a player given a name" do
     create = CreatesPlayer.new(name: "Dino_Master_Bob")
     creator.build
     expect(create.project.name).to eq("Dino_Master_Bob")
