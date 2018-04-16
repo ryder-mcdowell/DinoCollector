7e9dbf9b0b844f89c77ebf13e8e9f544c28e16bf
Curtis Baney <curtistheiii@gmail.com>
2018-04-16 11:22:44 -0700

    Refactor dino.rb and dino_spec.rb

--- app/models/dino.rb
+++ app/models/dino.rb
@@ -1,26 +1,15 @@
 class Dino
 
+  attr_accessor :name
+  attr_accessor :attack
+  attr_accessor :defence
+  attr_accessor :health
+
     def initialize(name, attack, defence, health)
       @name = name
       @attack = attack
       @defence = defence
       @health = health
-    end
-
-    def getName
-        @name
-    end
-
-    def getAttack
-        @attack
-    end
-
-    def getDefence
-        @defence
-    end
-
-    def getHealth
-        @health
     end
 
     def addHealth(health)
