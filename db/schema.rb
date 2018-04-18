43300e3190f9be76c3cc7b67aefbc1fa2202a372
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-18 13:20:02 -0700

    Comment out two pending helper specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416183907) do
+ActiveRecord::Schema.define(version: 20180416192926) do
+
+  create_table "dinos", force: :cascade do |t|
+    t.string "name"
+    t.string "attack"
+    t.string "defence"
+    t.string "health"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "name"
--- spec/system/new_player_spec.rb
+++ spec/system/new_player_spec.rb
@@ -1,15 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "creating a player", type: :system do
-  pending "allows a user to create a player with a name" do
+  it "allows a user to create a player with a name" do
     visit new_user_path
     fill_in "Player Name", with: "Dino_Master_Bob"
     click_on("Create New Player")
+    #byebug
     visit player_path
     expect(page).to have_content("Dino_Master_Bob")
   end
 
-  pending "do not allow user to create a project without a name" do
+  pending "does not allow user to create a project without a name" do
     visit new_user_path
     fill_in "Player Name", with: ""
     click_on("Create New Player")
