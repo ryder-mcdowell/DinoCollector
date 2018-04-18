ae2e58d02ea707cfb40894f40c74f323a6b91f3d
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-18 16:10:13 -0700

    Update player_spec to follow ApplicationRecord

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,15 +2,14 @@ require "rails_helper"
 
 RSpec.describe Player do
   let(:player) {Player.new}
-  let(:dino) {Dino.new("Raptor", 10, 10, 10)}
+  let(:dino) {Dino.new}
 
   it "doesn't initially have dinos" do
     expect(player.dino_count).to eq(0)
   end
 
   it "can gain more dinos" do
-    player.add_dino(dino)
-    expect(player.dino_count).to eq(1)
+    expect(player.dinos << dino).to change(player.dino_count).by(1)
   end
 
 end
