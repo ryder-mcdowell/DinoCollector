6342870cc67205bcbe6e62d0bef55d3dddb913a8
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-18 17:54:18 -0700

    Change name of click_on in new_player_spec

--- spec/system/new_player_spec.rb
+++ spec/system/new_player_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "creating a player", type: :system do
   it "allows a user to create a player with a name" do
     visit new_player_path
     fill_in "Player Name", with: "Dino_Master_Bob"
-    click_on("Create New Player")
+    click_on("Create Player")
     #byebug
     visit player_path
     expect(page).to have_content("Dino_Master_Bob")
@@ -13,7 +13,7 @@ RSpec.describe "creating a player", type: :system do
   pending "does not allow user to create a project without a name" do
     visit new_player_path
     fill_in "Player Name", with: ""
-    click_on("Create New Player")
+    click_on("Create Player")
     expect(page).to have_selector(".new_player")
   end
 end
