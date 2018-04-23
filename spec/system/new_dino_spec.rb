3458e62be5c17f3182eb604762fbeb697a4bb47a
Curtis Baney <curtistheiii@gmail.com>
2018-04-23 12:29:57 -0700

    Fix integration tests. Everything now passes

--- spec/system/new_dino_spec.rb
+++ spec/system/new_dino_spec.rb
@@ -17,13 +17,12 @@ RSpec.describe "creating a dino", type: :system do
   it "does not allow dinos to be created without a name" do
     visit new_dino_path
 
-    fill_in "Name", with: "trex"
+    fill_in "Name", with: ""
     fill_in "Attack", with: "10"
     fill_in "Defence", with: "10"
     fill_in "Health", with: "10"
 
     click_on("Create Dino")
-    visit dinos_path
     expect(page).to have_selector(".new_dino")
   end
 
