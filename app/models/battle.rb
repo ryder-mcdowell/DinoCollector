a91c3bf2f46f5e4341ccfebb7984f88df25a6576
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-25 16:01:22 -0700

    Add Battle fight method test

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,3 +1,11 @@
 class Battle
 
+def fight(contender, rival)
+  if contender.attack > rival.attack
+    return contender
+  else
+    return rival
+  end
+end
+
 end
--- spec/models/battle_spec.rb
+++ spec/models/battle_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Battle do
   let(:battle) {Battle.new}
+  let(:player_dino) {Dino.new(name: 'T Rex', attack: 10, defence: 10, health: 10)}
+  let(:enemy_dino) {Dino.new(name: 'Raptor', attack: 5, defence: 5, health: 5)}
+
+  it "can have a fight winner" do
+    expect(battle.fight(player_dino, enemy_dino)).to be_truthy
+  end
+
 end
