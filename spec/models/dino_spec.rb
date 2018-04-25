fe4f85fad7cbe7c503fe15bb61a4b0893bce9665
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-25 15:21:29 -0700

    Fix dino_spec.rb indentation to match ruby conventions

--- spec/models/dino_spec.rb
+++ spec/models/dino_spec.rb
@@ -1,36 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Dino, type: :model do
-    let(:startName) {'T Rex'}
-    let(:startAttack) {10}
-    let(:startDefence) {10}
-    let(:startHealth) {10}
-    let(:dino) {Dino.new(name: startName, attack: startAttack,
+  let(:startName) {'T Rex'}
+  let(:startAttack) {10}
+  let(:startDefence) {10}
+  let(:startHealth) {10}
+  let(:dino) {Dino.new(name: startName, attack: startAttack,
       defence: startDefence, health: startHealth)}
 
-    it "is has a name" do
-        expect(dino.name).to be_truthy
-    end
+  it "is has a name" do
+    expect(dino.name).to be_truthy
+  end
+  it "has an attack" do
+    expect(dino.attack).to be_truthy
+  end
+  it "has defence" do
+    expect(dino.defence).to be_truthy
+  end
+  it "has health" do
+    expect(dino.health).to be_truthy
+  end
 
-    it "has an attack" do
-        expect(dino.attack).to be_truthy
-    end
-
-    it "has defence" do
-        expect(dino.defence).to be_truthy
-    end
-
-    it "has health" do
-        expect(dino.health).to be_truthy
-    end
-
-    it "can add health" do
-        dino.addHealth(5)
-        expect(dino.health).to eq(startHealth+5)
-    end
-
-    it "can remove health" do
-        dino.removeHealth(5)
-        expect(dino.health).to eq(startHealth-5)
-    end
+  it "can add health" do
+    dino.addHealth(5)
+    expect(dino.health).to eq(startHealth+5)
+  end
+  it "can remove health" do
+    dino.removeHealth(5)
+    expect(dino.health).to eq(startHealth-5)
+  end
 end
