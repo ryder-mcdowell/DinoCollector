5a04e1fa1061c400f8896d591e8f849fc8d73fb8
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-29 21:56:06 -0700

    Make dino defence affect the amount of health lost in a fight

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -7,8 +7,8 @@ class Battle
 
   def attack_with_dino(player_dino)
     if player_dino.health > 1
-      @enemy_dino.removeHealth(player_dino.attack)
-      player_dino.removeHealth(@enemy_dino.attack)
+      @enemy_dino.removeHealth(player_dino.attack - @enemy_dino.defence)
+      player_dino.removeHealth(@enemy_dino.attack - player_dino.defence)
     else
       return
     end
--- spec/models/battle_spec.rb
+++ spec/models/battle_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Battle do
   let(:player) {Player.new}
-  let(:player_dino) {Dino.new(name: 'Raptor', attack: 20, defence: 20, health: 80)}
-  let(:enemy_dino) {Dino.new(name: 'T Rex', attack: 50, defence: 50, health: 150)}
+  let(:player_dino) {Dino.new(name: 'Raptor', attack: 20, defence: 0, health: 80)}
+  let(:enemy_dino) {Dino.new(name: 'T Rex', attack: 50, defence: 0, health: 150)}
   let(:battle) {Battle.new(player, enemy_dino)}
 
   skip "can have a fight winner" do
@@ -14,6 +14,12 @@ RSpec.describe Battle do
     battle.attack_with_dino(player_dino)
     expect(player_dino.health).to eq(30)
     expect(enemy_dino.health).to eq(130)
+  end
+
+  it "allows dino defence to reduce damage taken" do
+    enemy_dino.defence = 10
+    battle.attack_with_dino(player_dino)
+    expect(enemy_dino.health).to eq(140)
   end
 
   it "doesn't allow an unconcious dino to attack" do
