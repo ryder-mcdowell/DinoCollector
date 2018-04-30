121fff7a9b05c92be33cea59405045f40f4b8796
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-04-29 21:31:24 -0700

    Add Battle initializers (player and enemy dino)

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,11 +1,16 @@
 class Battle
 
-def fight(contender, rival)
-  if contender.attack > rival.attack
-    return contender
-  else
-    return rival
+  def initialize(player, enemy_dino)
+    @player = player
+    @enemy_dino = enemy_dino
   end
-end
+
+  def attack_with_dino(player_dino)
+    if player_dino.attack > @enemy_dino.attack
+      return player_dino
+    else
+      return @enemy_dino
+    end
+  end
 
 end
--- spec/models/battle_spec.rb
+++ spec/models/battle_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Battle do
-  let(:battle) {Battle.new}
-  let(:player_dino) {Dino.new(name: 'T Rex', attack: 10, defence: 10, health: 10)}
-  let(:enemy_dino) {Dino.new(name: 'Raptor', attack: 5, defence: 5, health: 5)}
+  let(:player) {Player.new}
+  let(:player_dino_strong) {Dino.new(name: 'Giga', attack: 1000, defence: 1000, health: 1000)}
+  let(:player_dino_weak) {Dino.new(name: 'Raptor', attack: 1, defence: 1, health: 1)}
+  let(:enemy_dino) {Dino.new(name: 'T Rex', attack: 100, defence: 100, health: 100)}
+  let(:battle) {Battle.new(player, enemy_dino)}
 
   it "can have a fight winner" do
-    expect(battle.fight(player_dino, enemy_dino)).to be_truthy
+    expect(battle.attack_with_dino(player_dino_weak)).to eq(enemy_dino)
   end
 
 end
