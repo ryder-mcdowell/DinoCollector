3104e626b6e51e5f6f6729f227d7fa3d854d0a92
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-05-02 16:16:48 -0700

    Remove all remaining logic from Battle that belongs in Dino

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -5,18 +5,7 @@ class Battle
     @enemy_dino = enemy_dino
   end
 
-  def attack_with_dino(player_dino)
-    return false unless player_dino.concious
-    @enemy_dino.damage(player_dino.attack - @enemy_dino.defence)
-    @enemy_dino.weaken(player_dino.attack)
-    player_dino.damage(@enemy_dino.attack - player_dino.defence)
-    player_dino.weaken(@enemy_dino.attack)
-  end
 
-  def dino_battle(player_dino)
-    while player_dino.concious && @enemy_dino.concious
-      attack_with_dino(player_dino)
-    end
-  end
+
 
 end
--- spec/models/battle_spec.rb
+++ spec/models/battle_spec.rb
@@ -6,38 +6,9 @@ RSpec.describe Battle do
   let(:enemy_dino) {Dino.new(name: 'T Rex', attack: 50, defence: 0, health: 150)}
   let(:battle) {Battle.new(player, enemy_dino)}
 
-  it "can have a fight winner" do
-    battle.dino_battle(player_dino)
+  pending "can have a fight winner" do
     expect(enemy_dino).to be_concious
     expect(player_dino).to_not be_concious
-  end
-
-  it "reduces the correct amount of health for the participating dinos" do
-    battle.attack_with_dino(player_dino)
-    expect(player_dino).to have_health(30)
-    expect(enemy_dino).to have_health(130)
-  end
-
-  it "allows dino defence to reduce damage taken" do
-    enemy_dino.defence = 10
-    battle.attack_with_dino(player_dino)
-    expect(enemy_dino).to have_health(140)
-  end
-
-  it "reduces a dinos defence by the amount of damage taken" do
-    enemy_dino.defence = 20
-    battle.attack_with_dino(player_dino)
-    expect(enemy_dino.defence).to eq(0)
-  end
-
-  it "will allow players to catch dinos if health is less then 10" do
-    player_dino.damage(71)
-    expect(player_dino.catchable?()).to be_truthy
-  end
-
-  it "will not allow players to catch dinos if they are to healthy" do
-    player_dino.damage(70)
-    expect(player_dino.catchable?).to_not be_truthy
   end
 
 end
