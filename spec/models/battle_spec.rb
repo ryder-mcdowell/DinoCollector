0c2338f34570a13f1d3769d58e0b0e854653033b
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-05-07 12:02:00 -0700

    Change Battle spec to be skipped instead of pending

--- spec/models/battle_spec.rb
+++ spec/models/battle_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Battle do
   let(:enemy_dino) {Dino.new(name: 'T Rex', attack: 50, defence: 0, health: 150)}
   let(:battle) {Battle.new(player, enemy_dino)}
 
-  pending "can have a fight winner" do
+  xit "can have a fight winner" do
     expect(enemy_dino).to be_concious
     expect(player_dino).to_not be_concious
   end
